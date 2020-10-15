d67a123343e09e514360fc137d0e884c682c4a5f
Ashley Trowell <ajtrowell@gmail.com>
2020-10-15 14:40:47 -0500

    Added placeholder for ddr4 memory write test
    Need to add psp commands to write a simple
    data sequence to ddr4 and read again to prove
    we can put values in memory where we want them.
    
    Demo is not expected to require modification of
    slx project.

---
+++ ddr4_initialize_test.m
@@ -0,0 +1,17 @@
+% Reference ZCU111 RFSoC HW/SW Co-design PDF
+% Pg 20-23
+% Libiio 
+% Reference ADC_IO_Capture_DDR4_Read.m for PSP commands
+
+%% Write to DDR4 Memory, step 1
+
+txSignal = 1:1000;
+
+
+
+
+
+%% Read directly from DDR4 Memory, step 2a
+% Reference ZCU111 RFSoC HW/SW Co-design PDF
+
+
