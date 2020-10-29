fcad6680b93faf60b220960247ca88591b711ebb
Ashley Trowell <ajtrowell@gmail.com>
2020-10-29 17:17:18 -0500

    data type mismatched ddr4 read and write using psps
    Two notes:
    The write dataLength (line 10) and the CaptureLength (line 11)
    should not be equal... but the correct ratio is unknown.
    
    The received data matches when the input is zero, but is
    scrambled for other inputs.  It is apparent that the datatype
    doesn't match.
    Needs to be casted or converted either before write or after
    read, or both.

--- ddr4_initialize_test.m
+++ ddr4_initialize_test.m
@@ -6,12 +6,56 @@
 % Clears the workspace:
 clear
 
-%% Write to DDR4 Memory, step 1
-
 % Creates a DATA vector from 1 to 1000; this is the devName (name of IIO device):
-DATA = 1:1000;
+dataLength = 100;
+CaptureLength = dataLength*8;
+% DATA = (1:dataLength);
+DATA = zeros(1,dataLength);
+% DATA = rand(1,dataLength);
 
+AddressOffset = 0;
 % Address which is the starting address offset:
+IPAddr = 'ip:192.168.1.101';
+rd = pspshared.libiio.sharedmem.read('IPAddress',IPAddr,'DataType','int16');
+wr = pspshared.libiio.sharedmem.write('IPAddress',IPAddr);
+
+%% Write to DDR4 Memory, step 1
+wr(AddressOffset,DATA)
+
+%% Read directly from DDR4 Memory, step 2a
+% Reference ZCU111 RFSoC HW/SW Co-design PDF
+data_rd_1 = rd(AddressOffset,CaptureLength); % read 2 gigs out, 8 or how many int16 samples in 128
+
+
+%% Cleanup Data and plot
+
+% Example from ADC_IO_Capture_DDR4_Read.m
+% Unpack data from word-ordering in memory 
+% (set in ADC_Capture_4x4_IQ_DDR4/HDL_IP/DDR_Capture_Logic/DataBusBreakout)
+% will no longer work
+% temp = reshape(data_rd_1, 4, []);
+% data_i = reshape(temp(:,1:2:end),[],1);
+% data_q = reshape(temp(:,2:2:end),[],1);
+% data = complex(data_i,data_q);
+
+figure(1)
+subplot(2,1,1)
+plot(DATA,'*')
+title('Input Data')
+subplot(2,1,2)
+plot(data_rd_1,'*')
+title('Output Data')
+
+%% Cleanup
+release(rd) %releases shared memory object reader
+
+%{
+% AXI4 MM IIO Write registers
+AXI4_ADC_SelectCh =  pspshared.libiio.aximm.write(...
+                   'IPAddress',IPAddr,...
+                   'AddressOffset',hex2dec('11C')); 
+                        
+
 ADDR = 'ip:192.168.1.101';
 
 % Returns a Shared Memory Write system object (AXI4_DDR4_Write), that provides access to a
@@ -23,10 +67,5 @@ AXI4_DDR4_Write = pspshared.libiio.sharedmem.write(...
 % Copies a buffer of data (txSignal), to the memory region starting at
 % address offset ADDR:
 step(AXI4_DDR4_Write, ADDR, DATA) % how does this compare to the release method?
-
-
-
-%% Read directly from DDR4 Memory, step 2a
-% Reference ZCU111 RFSoC HW/SW Co-design PDF
-
+%}
 
