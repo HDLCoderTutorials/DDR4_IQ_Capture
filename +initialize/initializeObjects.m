c8e2c692c8945a2515d23acc160072042f699f79
Ashley Trowell <ajtrowell@gmail.com>
2020-10-31 11:21:04 -0500

    Converted initializeObjects to structure syntax
    Structure syntax makes it easier to write input properties
    in terms of one another.

--- +initialize/initializeObjects.m
+++ +initialize/initializeObjects.m
@@ -1,32 +1,31 @@
 function output = initializeObjects()
 % Generates structure of initialization objects using pl_config package
-synthesisCellArray = {...
-    'fpga_clock_rate_hz',128e6,...
-    'sample_rate_hz',512e6,...
-    'N_accumulator',14};
-output.synthesisConfig = pl_config.SynthesisConfig(synthesisCellArray{:});
+
+%% Synthesis Config Initialization
+scStruct.fpga_clock_rate_hz = 128e6;
+scStruct.sample_rate_hz = 512e6;
+scStruct.N_accumulator = 14;
+output.synthesisConfig = pl_config.SynthesisConfig(scStruct);
 assert(output.synthesisConfig.isValid(),'Sythesis Config Failed validation.')
 
-radarSetupCellArray = {...
-    'pulses_per_cpi',128,...
-    'pulse_width_sec',5e-6,...
-    'prf_hz',10e3,...
-    'scene_start_m',(5e-6 + 1/output.synthesisConfig.fpga_clock_rate_hz)* pl_config.RadarSetup.c/2 ,... % pulse width plus 1 cycle to match original
-    'range_swath_m', (5e-6)*4 * pl_config.RadarSetup.c ,...
-    'chirp_start_frequency_hz',0e6,...
-    'chirp_stop_frequency_hz',140e6,...
-    'pl_synthesis_config',output.synthesisConfig};
-output.radarSetup = pl_config.RadarSetup(radarSetupCellArray{:});
+%% Radar Setup Initialization
+rsStruct.pulses_per_cpi = 128;
+rsStruct.pulse_width_sec = 5e-6;
+rsStruct.prf_hz = 10e3;
+rsStruct.scene_start_m = (rsStruct.pulse_width_sec + ...
+    1/output.synthesisConfig.fpga_clock_rate_hz) * pl_config.RadarSetup.c/2 ;
+rsStruct.range_swath_m =  4 * (rsStruct.pulse_width_sec) * pl_config.RadarSetup.c ;
+rsStruct.chirp_start_frequency_hz = 0e6;
+rsStruct.chirp_stop_frequency_hz = 140e6;
+rsStruct.pl_synthesis_config = output.synthesisConfig;
+output.radarSetup = pl_config.RadarSetup(rsStruct);
 assert(output.radarSetup.isValid(),'Radar Setup Failed validation.')
 
 % output.radarSetup.getRadarPerformance()
 % output.radarSetup.plot()
+%% Register Config Initialization
 % This pl_config is the output for programming the fpga programable logic.
 output.pl_register_config = output.radarSetup.getRegisterConfig();
 assert(output.pl_register_config.isValid(),'PL Register Config Failed validation.')
 
 
-
-% Convert name value cell aray argument lists into structures.
-synthesisStruct = cell2struct(synthesisCellArray(2:2:end),synthesisCellArray(1:2:end),2);
-radarSetupStruct = cell2struct(radarSetupCellArray(2:2:end),radarSetupCellArray(1:2:end),2);
