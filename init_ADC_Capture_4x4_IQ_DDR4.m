dff8be917b599b4337bd2ba9f308b8347ee1fc87
Ashley Trowell <ajtrowell@gmail.com>
2020-11-01 00:13:26 -0600

    Removed non OOP initialization, added waveform to Presets

--- init_ADC_Capture_4x4_IQ_DDR4.m
+++ init_ADC_Capture_4x4_IQ_DDR4.m
@@ -1,85 +1,12 @@
-%% Initialize parameters
 clearvars
-clearvars requiredVars
-requiredVars = {};
-
-ConverterSamplingRate = 2048e6;
-DDC_DUC_factor = 4;
-
-% requiredVars{end+1} = 'Fs';
-Fs = ConverterSamplingRate/DDC_DUC_factor;
-Ts = 1/Fs; 
-
-% requiredVars{end+1} = 'VectorSamplingFactor';
-VectorSamplingFactor = 4;
-
-fpga_clk_rate = Fs/VectorSamplingFactor;
-fpga_Ts = 1/fpga_clk_rate;
-
-%% Pulse parameters
-% requiredVars{end+1} = 'CPILength';
-CPILength = 128; % pulses per CPI
-PRF = 10000; %Hz
-PulseWidth = 5e-6; % seconds
-RngGateDelay = 1/fpga_clk_rate; % time in seconds to delay after Tx start before Rx start
-RngSwathLength = 4*PulseWidth * 1.25; % time in seconds of RX data to save each pulse, for now set to match 4*PulseWidth + 25%
-
-%% CHIRP parameters
-
-% requiredVars{end+1} = 'PulseWidth_count';
-% PulseWidth_count = PulseWidth*fpga_clk_rate;
-
-% requiredVars{end+1} = 'RngGateDelay_count';
-RngGateDelay_count =RngGateDelay*fpga_clk_rate; % seconds 
-% requiredVars{end+1} = 'RngSwathLength_count';
-RngSwathLength_count = RngSwathLength*fpga_clk_rate; % seconds * clocks per sec = fpga clock cycles
-
-% requiredVars{end+1} = 'CaptureLength';
-% CaptureLength = CPILength*RngSwathLength_count; % pulse_count * fpgaClockCycle_count
-
-
-%% Cleanup parameters, testing to see which parameters are required.
-
-requiredVars{end+1} = 'requiredVars'; % To aid debugging
-clearvars('-except',requiredVars{:}) % Clears all non-required vars
-params.original = utilities.v2struct([{'fieldNames'},requiredVars]);
-clearvars('-except','requiredVars','params')
-
-% This clearvars line is used to clear all variables from the workspace
-% that aren't found in the clearvars cellarray. The objective is to verify
-% that the requiredVars array has correctly identified ALL variables
-% required for initialization and synthesis, which means these are the only
-% vairables that a object oriented interface must provide in order to run
-% everything.  Once these variables are understood, initialization
-% functionality, including error checking and performance prediction, 
-% will be moved to the +pl_config.RadarSetup Class and this
-% script will be cleaned up.
-
-
+% ConverterSamplingRate = 2048e6;
+% DDC_DUC_factor = 4;
 
 %% OOP Initialization
-% Generates all object oriented initialization objects, though 
-% not guaranteed to have the same input/initialization
-initObjects = initialize.initializeObjects();
-
-% Populate required variables from object oriented initialization
-% Fs = initObjects.synthesisConfig.sample_rate_hz; % Read.m
-% VectorSamplingFactor = initObjects.synthesisConfig.samples_per_clock_cycle; % slx  'factor' in 'ADC_Capture_4x4_IQ_DDR4/HDL_IP/NCO_Transmit1/Vectorized NCO'
-% CPILength = initObjects.radarSetup.pulses_per_cpi; % Read,  'Value' in 'ADC_Capture_4x4_IQ_DDR4/Constant2,  'Value' in 'ADC_Capture_4x4_IQ_DDR4/HDL_IP/DefaultRegister6/Constant5' 
-% N = initObjects.synthesisConfig.N_accumulator; % all over slx,
-% PRI_count = initObjects.registerConfig.pri_cycles;
-% PulseWidth_count = initObjects.registerConfig.pulse_width_cycles;
-% RngGateDelay_count = initObjects.registerConfig.tx_end_to_rx_start_delay_cycles;
-
-% Seems redundant with CaptureLength... what was the difference?
-% RngSwathLength_count = initObjects.registerConfig.range_swath_cycles;
-% What are CaptureLength's units?  fpga cycles? Samples?  IDK...
-% CaptureLength        = initObjects.registerConfig.range_swath_cycles ...
-%     * initObjects.radarSetup.pulses_per_cpi; % Read.m, 
-% start_inc = initObjects.registerConfig.start_inc_steps; % compare values?
-% end_inc = initObjects.registerConfig.end_inc_steps; % compare values?
-% LFM_counter_inc = initObjects.registerConfig.lfm_counter_inc; % Calculation seems to match
-
+initObjects = initialize.Presets.originalScript;
+initObjects.radarSetup.plot;
+initObjects.radarSetup
+initObjects.radarSetup.getRadarPerformance
 
 %% DDR plant model param - might be acceptable, should be in a structure
 DDR.DataWidth = 128;
@@ -97,22 +24,4 @@ sim_CaptureLength = initObjects.registerConfig.ddr4_samples;
 sim_RdFrameSize = 64;
 sim_RdNumFrames = ceil(sim_CaptureLength/sim_RdFrameSize);
 
-
-%% Compare original and final parameter calculations
-params.final = utilities.v2struct([{'fieldNames'},requiredVars]);
-% Create comparison cell array
-params.comparison = requiredVars;
-params.unequal = {};
-for ii=1:numel(requiredVars)
-    params.comparison{2,ii} = params.original.(params.comparison{1,ii});
-    params.comparison{3,ii} = params.final.(params.comparison{1,ii});
-    if (isa(params.comparison{2,ii},'numeric') && isa(params.comparison{3,ii},'numeric'))
-        if ( params.comparison{2,ii} ~= params.comparison{3,ii} )
-            params.unequal(1:3,end+1) = params.comparison(1:3,ii);
-        end
-    end
-end
-
-disp(params.unequal)
-% Compile slx
-% ADC_Capture_4x4_IQ_DDR4([], [], [], 'compile')
+% ADC_Capture_4x4_IQ_DDR4([], [], [], 'compile') % Compile slx
