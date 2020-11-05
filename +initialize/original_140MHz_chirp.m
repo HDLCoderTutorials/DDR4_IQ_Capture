5c1a67d92b4e48b95ded78cfa1e094f946bd762c
Ashley Trowell <ajtrowell@gmail.com>
2020-11-05 13:23:04 -0600

    renamed initObjects > config

--- +initialize/original_140MHz_chirp.m
+++ +initialize/original_140MHz_chirp.m
@@ -1,31 +1,31 @@
-function output = initializeObjects()
+function config = initializeObjects()
 % Generates structure of initialization objects using pl_config package
 
 %% Synthesis Config Initialization
 scStruct.fpga_clock_rate_hz = 128e6;
 scStruct.sample_rate_hz = 512e6;
 scStruct.N_accumulator = 14;
-output.synthesisConfig = pl_config.SynthesisConfig(scStruct);
-assert(output.synthesisConfig.isValid(),'Sythesis Config Failed validation.')
+config.synthesisConfig = pl_config.SynthesisConfig(scStruct);
+assert(config.synthesisConfig.isValid(),'Sythesis Config Failed validation.')
 
 %% Radar Setup Initialization
 rsStruct.pulses_per_cpi = 128;
 rsStruct.pulse_width_sec = 5e-6;
 rsStruct.prf_hz = 10e3;
 rsStruct.scene_start_m = (rsStruct.pulse_width_sec + ...
-    1/output.synthesisConfig.fpga_clock_rate_hz) * pl_config.RadarSetup.c/2 ;
+    1/config.synthesisConfig.fpga_clock_rate_hz) * pl_config.RadarSetup.c/2 ;
 rsStruct.range_swath_m =  4 * (rsStruct.pulse_width_sec) * pl_config.RadarSetup.c ;
 rsStruct.chirp_start_frequency_hz = 0e6;
 rsStruct.chirp_stop_frequency_hz = 140e6;
-rsStruct.pl_synthesis_config = output.synthesisConfig;
-output.radarSetup = pl_config.RadarSetup(rsStruct);
-assert(output.radarSetup.isValid(),'Radar Setup Failed validation.')
+rsStruct.pl_synthesis_config = config.synthesisConfig;
+config.radarSetup = pl_config.RadarSetup(rsStruct);
+assert(config.radarSetup.isValid(),'Radar Setup Failed validation.')
 
-% output.radarSetup.getRadarPerformance()
-% output.radarSetup.plot()
+% config.radarSetup.getRadarPerformance()
+% config.radarSetup.plot()
 %% Register Config Initialization
-% This pl_config is the output for programming the fpga programable logic.
-output.registerConfig = output.radarSetup.getRegisterConfig();
-assert(output.registerConfig.isValid(),'PL Register Config Failed validation.')
+% This pl_config is the config for programming the fpga programable logic.
+config.registerConfig = config.radarSetup.getRegisterConfig();
+assert(config.registerConfig.isValid(),'PL Register Config Failed validation.')
 
 
--- init_ADC_Capture_4x4_IQ_DDR4.m
+++ init_ADC_Capture_4x4_IQ_DDR4.m
@@ -3,24 +3,24 @@ clearvars
 % DDC_DUC_factor = 4;
 
 %% OOP Initialization
-initObjects = initialize.original_140MHz_chirp;
-% initObjects.radarSetup.plot;
-initObjects.radarSetup
-% initObjects.radarSetup.getRadarPerformance
+config = initialize.original_140MHz_chirp;
+% config.radarSetup.plot;
+config.radarSetup
+% config.radarSetup.getRadarPerformance
 
 %% DDR plant model param - might be acceptable, should be in a structure
 DDR.DataWidth = 128;
-DDR.Depth = (initObjects.registerConfig.range_swath_cycles ...
-    * initObjects.radarSetup.pulses_per_cpi) ...
+DDR.Depth = (config.registerConfig.range_swath_cycles ...
+    * config.radarSetup.pulses_per_cpi) ...
     / (DDR.DataWidth/(2*16)) * 1.25; % Number of 16 bit I and Q samples collect + 25%
 DDR.DataType = fixdt(0,DDR.DataWidth,0);
 DDR.InitData = fi(zeros(1,DDR.Depth),DDR.DataType);
 
-%% Sim parameters - should be replaced by a second initObjects structure
+%% Sim parameters - should be replaced by a second config structure
 % with simulation specific changes.
 % Simulation specific parameters are fine, but they should be passed in 
 % through a uniform interface, instead of being another random global var.
-sim_CaptureLength = initObjects.registerConfig.ddr4_samples;
+sim_CaptureLength = config.registerConfig.ddr4_samples;
 sim_RdFrameSize = 64;
 sim_RdNumFrames = ceil(sim_CaptureLength/sim_RdFrameSize);
 
